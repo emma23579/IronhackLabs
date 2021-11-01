1e48e5d4e0ff797a2ecaf288bf100a67bf3685da
emma23579 <82088065+emma23579@users.noreply.github.com>
2021-11-01 17:13:15 +0100

    Add files via upload

---
+++ SQL_Intro_Lab.sql
@@ -0,0 +1,18 @@
+select * from sakila.actor;
+select * from sakila.address;
+select * from sakila.category;
+select * from sakila.city;
+select * from sakila.country;
+
+select title from sakila.film_text;
+
+select * from sakila.language;
+select distinct name as "language" from sakila.language;
+
+select * from sakila.store;
+
+select * from sakila.staff;
+
+select first_name from sakila.staff;
+
+
